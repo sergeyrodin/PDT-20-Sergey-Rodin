d6c1eda11b47e50747e94baf682bf657715bf918
sergeyrodin <sergey.rodin@donriver.com>
2014-10-24 02:41:17 +0400

    DZ4

---
+++ src/com/example/tests/ContactModificationTests.java
@@ -0,0 +1,18 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactModificationTests extends TestBase{
+	
+	@Test
+	public void modifySomeContact() throws Exception {
+		app.getNavigationHelper().openMainPage();
+		app.getContactHelper().initContactModification(1);
+		ContactData contact = new ContactData();
+		contact.first_name = "new first name";
+		app.getContactHelper().fillContactForm(contact);
+		app.getContactHelper().submitContactModification();
+	    app.getNavigationHelper().openMainPage();	
+
+	}
+}
