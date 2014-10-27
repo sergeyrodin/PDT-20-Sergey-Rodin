9e6111c2f3ace013db81e3ae11bf1117d4614803
sergeyrodin <sergey.rodin@donriver.com>
2014-10-27 18:54:54 +0400

    inaccuracies correction

--- src/com/example/fw/HelperBase.java
+++ src/com/example/fw/HelperBase.java
@@ -52,7 +52,7 @@ public abstract class HelperBase {
 	  }
 
 	protected void type(By locator, String text) {
-		if (text != null) {
+		if (text == null || text == "") {
 			driver.findElement(locator).clear();
 			driver.findElement(locator).sendKeys(text);
 		}
@@ -63,7 +63,7 @@ public abstract class HelperBase {
 	}
 
 	protected void selectByText(By locator, String text) {
-		if (text != null) {
+		if (text == null || text == "") {
 		new Select(driver.findElement(locator)).selectByVisibleText(text);
 	    }
 	}
